48288aa77ed91971b6240c69c7e2f012487ee1cf
dabde <34655672+dabde@users.noreply.github.com>
2021-06-24 08:43:05 +0200

    Metal 2665/add monitoring (#3)
    * METAL-2665 add monitoring
    
    * METAL-2665 small fixes
    
    * METAL-2665 add Readme

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,11 @@
 FROM postgres:11-alpine
-ARG CLOUD_SDK_VERSION=276.0.0
+ARG CLOUD_SDK_VERSION=339.0.0
 ENV CLOUD_SDK_VERSION=$CLOUD_SDK_VERSION
 
 ENV PATH /google-cloud-sdk/bin:$PATH
 RUN apk --no-cache add \
         curl \
-        python \
+        python3 \
         py-crcmod \
         bash \
         libc6-compat \
--- entrypoint.sh
+++ entrypoint.sh
@@ -3,12 +3,14 @@ set -e
 
 echo "Prepare configuration for script"
 TIMESTAMP=$(date +%F_%R)
+START_TIMESTAMP=$(date +%s)
 BACKUP_FILE=${DB_NAME}-${TIMESTAMP}.sql.gz
 BACKUP_FILE_LATEST=${DB_NAME}-latest.sql.gz
 DB_HOST=${DB_HOST:-localhost}
 DB_PASSWORD=$(cat ${DB_PASSWORD_FILE})
 DB_USER=$(cat ${DB_USERNAME_FILE})
 CREDENTIALFILE=${CREDENTIALFILE:-/srv/gcloud/credentials.json}
+PROM_NAMESPACE=${PROM_NAMESPACE:-kci}
 
 if [ ! -f ${CREDENTIALFILE} ]
 then
@@ -21,18 +23,31 @@ echo "login to gcloud with SA"
 gcloud auth activate-service-account --key-file=/srv/gcloud/credentials.json
 
 # create login credential file
-echo *:5432:*:${DB_USER}:${DB_PASSWORD} >> ~/.pgpass
-chmod 0600 ~/.pgpass
+(umask 377 && echo *:5432:*:${DB_USER}:${DB_PASSWORD} >> ~/.pgpass)
 
 echo "Start create backup"
 pg_dump -F c -Z 9 -h ${DB_HOST} -p 5432 -U ${DB_USER} ${DB_NAME} -f ${BACKUP_FILE}
+BACKUP_SIZE=$(du ${BACKUP_FILE} | awk '{print $1}')
 echo "End backup"
 
 ## copy to destination
 echo "Copy to gcs"
 gsutil cp ${BACKUP_FILE} gs://${GCS_BUCKET}/${DB_NAME}/${BACKUP_FILE} && gsutil cp ${BACKUP_FILE} gs://${GCS_BUCKET}/${DB_NAME}/${BACKUP_FILE_LATEST}
 
-if test $? -ne 0 
+END_TIMESTAMP=$(date +%s)
+BACKUP_DURATION=$((END_TIMESTAMP - START_TIMESTAMP))
+if [[ ! -z "$PROMETHEUS_PUSH_GATEWAY" ]];
 then
-	exit 1;
+echo "sending monitoring metrics to ${PROMETHEUS_PUSH_GATEWAY}"
+cat <<EOF | curl -s --data-binary @- http://${PROMETHEUS_PUSH_GATEWAY}/metrics/job/pgdump-gcs/source_type/postgresql/source_name/${DB_NAME}
+    # TYPE ${PROM_NAMESPACE}_backup_timestamp counter
+    # HELP ${PROM_NAMESPACE}_backup_timestamp Timestamp of last backup run
+    ${PROM_NAMESPACE}_backup_timestamp $END_TIMESTAMP
+    # TYPE ${PROM_NAMESPACE}_backup_duration gauge
+    # HELP ${PROM_NAMESPACE}_backup_duration Time the backup run take until finished
+    ${PROM_NAMESPACE}_backup_duration $BACKUP_DURATION
+    # TYPE ${PROM_NAMESPACE}_backup_size gauge
+    # HELP ${PROM_NAMESPACE}_backup_size Backup Size in bytes
+    ${PROM_NAMESPACE}_backup_size $BACKUP_SIZE
+EOF
 fi
